1e739279614cca6d61cfdc968b0576170db782c6
Martin Sandström <martin@marteinn.se>
2019-04-07 18:43:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Giraffe-Docker-Boilerplate
+
+This is a docker based Giraffe boilerplate that uses the giraffe-template to give you a fresh project to get started with.
+
+
+## Clean install
+
+1. Copy configuration
+
+    ```
+    cp docker/config/web.example.env docker/config/web.env
+    ```
+
+2. Open `docker/config/web.env` and change `SCAFFOLD_APP_NAME=` to the otp name you want for your app
+3. Run: docker-compose up
+4. Done. Visit [localhost:5000](http://localhost:5000)
+
+
+## License
+
+Giraffe-Docker-Boilerplate is released under the [MIT License](http://www.opensource.org/licenses/MIT).
---
+++ src/docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+# $0 is a script name, $1, $2, $3 etc are passed arguments
+# $1 is our command
+# Credits: https://rock-it.pl/how-to-write-excellent-dockerfiles/
+CMD=$1
+
+setup_giraffe() {
+    echo "Scaffold project"
+
+    # It yields "No such file or directory" on nix*, so ignore it and continue
+    dotnet new giraffe -o $SCAFFOLD_APP_NAME || true
+
+    echo "Move app"
+    cp -Rp ./$SCAFFOLD_APP_NAME/. .
+    rm -rf ./$SCAFFOLD_APP_NAME
+
+    echo "Pin aspnetcore to 2.1.3 for project"
+    sed -i -E 's/Microsoft.AspNetCore.App.*Version="2.1.."/Microsoft.AspNetCore.App" Version="2.1.3"/g' src/$SCAFFOLD_APP_NAME/$SCAFFOLD_APP_NAME.fsproj
+
+    echo "Install packages"
+    chmod +x build.sh
+    ./build.sh
+}
+
+
+case "$CMD" in
+    "runserver" )
+        if [ ! -d ./$SCAFFOLD_APP_NAME ]; then
+            setup_giraffe
+        fi
+
+        echo "Run server"
+        ASPNETCORE_URLS="http://0.0.0.0:5000" dotnet run --project src/$SCAFFOLD_APP_NAME
+        ;;
+    * )
+        # Run custom command. Thanks to this line we can still use
+        # "docker run our_container /bin/bash" and it will work
+        exec $CMD ${@:2}
+        ;;
+esac
